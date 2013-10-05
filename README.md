8d65adb9600a7b507fe7733c51a430e74c1901f9
Sebastian <sebasporto@gmail.com>
2013-10-06 04:01:25 +1100

    Added Yo Angular

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-kic
+KIC
 ===
 
 Kids Investment Company
+
+
+Dev
+----
+
+
+Testing
+-------
+
+
+Install gorc
+
+  go get github.com/stretchr/gorc
+
+  gorc
---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('kicApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/home', {
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
