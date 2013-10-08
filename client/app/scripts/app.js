6da77abde0224970c338d894d64251b157e590aa
Sebastian <sebasporto@gmail.com>
2013-10-08 22:54:56 +1100

    Added Restangular

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -1,17 +1,26 @@
 'use strict';
 
-angular.module('APP', [])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/accounts', {
-        templateUrl: 'views/accounts.html',
-        controller: 'AccountsIndexCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+log.setLevel('info');
+
+var app = angular.module('APP', ['restangular']);
+
+app.constant('apiBase', '//localhost:5000');
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/accounts', {
+      templateUrl: 'views/accounts/index.html',
+      controller: 'AccountsIndexCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+app.run(function(Restangular, apiBase) {
+  Restangular.setBaseUrl(apiBase);
+});
--- client/app/scripts/controllers/accounts.js
+++ client/app/scripts/controllers/accounts.js
@@ -1,10 +1,20 @@
 'use strict';
 
 angular.module('APP')
-  .controller('AccountCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('AccountsIndexCtrl', function ($scope, Account, logger, notifier) {
+    logger.info('Getting accounts');
+    notifier.success('Getting accounts');
+
+    Account.getList()
+      .then(function (accounts) {
+        logger.info('success');
+        logger.info(accounts);
+        notifier.success(accounts);
+      }, 
+      function errorCallback(err) {
+        logger.info('error');
+        logger.info(err);
+        notifier.error(err);
+      });
+
   });
