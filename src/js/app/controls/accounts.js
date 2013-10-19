4c08c3c7e95b8193fdaee63db244b9c304b9cdf6
Sebastian <sebasporto@gmail.com>
2013-10-19 11:59:14 +1100

    Accounts

--- src/js/app/controls/accounts.js
+++ src/js/app/controls/accounts.js
@@ -4,15 +4,19 @@ define(['can', 'app/models/account'], function (can, Account) {
 
 	return can.Control({
 		init: function (ele, options) {
-			this.accounts = Account.List([]);
+			var self = this;
+
+			this.accounts = new Account.List([]);
 			this.state = can.Map({
 
 			});
 
 			this.element.html(can.view('accounts-template', {accounts: this.accounts, state: this.state}));
 
-			console.log('getting accounts')
+			console.log('getting accounts');
+
 			Account.findAll({}, function (accounts) {
+				console.log('Got accounts')
 				console.log(accounts);
 				self.accounts.replace(accounts);
 			}, function (xhr) {
--- src/js/app/models/account.js
+++ src/js/app/models/account.js
@@ -3,7 +3,8 @@ define(['can'], function (can) {
 	"use strict";
 
 	return can.Model({
+		models: 'd',
 		findAll: 'GET /api/accounts'
-	});
+	}, {});
 
 });
