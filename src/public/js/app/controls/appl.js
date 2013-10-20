e9e61165c112b3110c0df1842effde8554eff9cc
Sebastian <sebasporto@gmail.com>
2013-10-20 17:37:56 +1100

    Some more routing

--- src/public/js/app/controls/appl.js
+++ src/public/js/app/controls/appl.js
@@ -1,20 +1,36 @@
 define(['logger', 'can', 'js/app/controls/router', 'js/app/controls/accounts'], function(logger, can, Router, AccountsCtrl) {
 
 	'use strict';
-	
+
 	logger.info('ApplControl loaded');
 
 	return can.Control({
 		defaults: {
 			view: 'app-template'
 		}
-	},{
-		init: function (el, options) {
-			var state = can.Map({title: 'Hello'});
+	}, {
+		init: function(el, options) {
+			var state = can.Map({
+				title: 'Hello'
+			});
 			this.element.append(can.view(this.options.view, state));
 
-			new Router(document);
 			new AccountsCtrl($('.accounts', this.element));
+
+			can.route.ready(false);
+
+		},
+
+		'accounts route': 'accounts',
+		'accounts/:id route': 'account',
+
+		accounts: function () {
+			logger.info('accounts');
+		},
+
+		account: function (data) {
+			logger.info('route accounts/id');
+			logger.info(data.id);
 		},
 
 	});
